c919ab9fef21173f4cdb761a7998b326d0506b24
DIR\sebastiano.mura <murasebastiano87@gmail.com>
2016-12-02 18:02:07 +0100

    Prima stesura HTML (versione statica) e prime bozze di style

---
+++ imbd.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+        <title>Imbd</title>
+
+        <!-- Bootstrap--> 
+        <link href="node_modules/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/style.css" rel="stylesheet">
+
+        <!--additional fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+
+           <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+  <body>
+    <header>
+        <nav class="navbar navbar-default">
+          <div class="container-fluid">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="#">
+                    <img class="img-responsive" alt="Brand" src="img/imbd-logo.jpg"> 
+                </a>
+                <ul>
+                    <li><a>Movies</a></li>
+                    <li><a>Celebs & Photos</a></li>
+                    <li><a>Community</a></li>
+                    <li><a>News</a></li>
+                </ul>
+
+            </div>
+          </div>
+        </nav>
+
+        <div class="title">
+            <h3>The martian</h3>
+            <h4>Adventure Sci-fi Thriller</h4>
+            <!--bottone con modale-->
+            <button>trailer</button>
+            <p>In Theaters <br>
+            <span>15, Oct 2015 (USA)</span>
+            </p>
+        </div>
+    </header>
+
+    <main>
+        <div class="container selection">
+            <div class="row">
+                <div class="navbar">
+                 <ul>
+                  <li>In theaters<li>
+                  <li>Coming soon<li>
+                  <li>Charts<li>
+                  <li>TV Series<li>
+                  <li>Trailers<li>
+                  <li>More</li>
+                </div>
+                <div class="white"></div>
+                <div class="col-sm-4">
+                     <img class="img-responsive" src="img/locandina.jpg">
+                     <h4>Mad max: Fury road</h4>
+                     <h5>Action, adventure, Sci-Fi</h5>
+                </div>
+                <div class="col-sm-4">
+                     <img class="img-responsive" src="img/locandina.jpg">
+                     <h4>Mad max: Fury road</h4>
+                     <h5>Action, adventure, Sci-Fi</h5>
+                 </div>
+                <div class="col-sm-4">
+                     <img class="img-responsive" src="img/locandina.jpg">
+                     <h4>Mad max: Fury road</h4>
+                     <h5>Action, adventure, Sci-Fi</h5>
+                 </div>
+                <div class="col-sm-4">
+                     <img class="img-responsive" src="img/locandina.jpg">
+                     <h4>Mad max: Fury road</h4>
+                     <h5>Action, adventure, Sci-Fi</h5>
+                 </div>
+                <div class="col-sm-4">
+                     <img class="img-responsive" src="img/locandina.jpg">
+                     <h4>Mad max: Fury road</h4>
+                     <h5>Action, adventure, Sci-Fi</h5>
+                 </div>
+                <div class="col-sm-4">
+                     <img class="img-responsive" src="img/locandina.jpg">
+                     <h4>Mad max: Fury road</h4>
+                     <h5>Action, adventure, Sci-Fi</h5>
+                 </div>
+             </div>
+        </div>
+    </main>
+    <div></div>
+    <footer>
+        <a><img src="#"></a>
+        <a><img src="#"></a>
+        <a><img src="#"></a>
+        <a><img src="#"></a>
+        <a><img src="#"></a>
+    </footer>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="node_modules/jquery/dist/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+
+  </body>
+</html>
---
+++ imbd2.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Imbd</title>
+
+    <!-- Bootstrap--> 
+    <link href="node_modules/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+
+    <!--additional fonts-->
+
+       <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <header>
+     <!-- Questo div conterrà la navbar, i diversi div testuali, i loghi i bottoni -->
+        <div class="container-fluid"> 
+            <a id="logo"><img src="img/imbd-logo.jpg"></a>
+
+            <!--carousel
+            <div class="container-fluid">
+                <div id="carousel-example-vertical" class="carousel vertical slide">
+                  <div class="carousel-inner" role="listbox">
+                    <div class="item active">
+                        <img class="img-responsive" src="img/themartian.jpg"> 
+                        <div class="title">
+                            <h3>The martian</h3>
+                            <h4>Adventure Sci-fi Thriller</h4>
+                bottone con modale
+                            <button>trailer</button>
+                            <p>In Theaters <br>
+                                <span>15, Oct 2015 (USA)</span>
+                            </p>
+                        </div>  
+                    </div>
+                    <div class="item">
+                        <img src="img/themartian.jpg">
+                    </div>
+                    <div class="item">
+                        <img src="img/themartian.jpg">
+                      </p>
+                    </div>
+                    <div class="item">
+                        <img src="img/themartian.jpg">
+                    </div>
+                    <div class="item">
+                        <img src="img/themartian.jpg">
+                    </div>
+                  </div>
+                </div>                                
+              <a class="up carousel-control" href="#carousel-example-vertical" role="button" data-slide="prev">
+                <span class="glyphicon glyphicon-chevron-up" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+              </a>
+              <a class="down carousel-control" href="#carousel-example-vertical" role="button" data-slide="next">
+                <span class="glyphicon glyphicon-chevron-down" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+              </a>
+            </div>-->
+
+            <div>
+            </div>
+  
+        </div>
+    </header>
+
+    <main>
+        <div class="container selection">
+            <div class="row">
+                <div class="navbar">
+                 <ul>
+                  <li>In theaters<li>
+                  <li>Coming soon<li>
+                  <li>Charts<li>
+                  <li>TV Series<li>
+                  <li>Trailers<li>
+                  <li>More</li>
+                </div>
+                <div class="white"></div>
+                <div class="col-sm-4">
+                     <img class="img-responsive" src="img/locandina.jpg">
+                     <h4>Mad max: Fury road</h4>
+                     <h5>Action, adventure, Sci-Fi</h5>
+                </div>
+                <div class="col-sm-4">
+                     <img class="img-responsive" src="img/locandina.jpg">
+                     <h4>Mad max: Fury road</h4>
+                     <h5>Action, adventure, Sci-Fi</h5>
+                 </div>
+                <div class="col-sm-4">
+                     <img class="img-responsive" src="img/locandina.jpg">
+                     <h4>Mad max: Fury road</h4>
+                     <h5>Action, adventure, Sci-Fi</h5>
+                 </div>
+                <div class="col-sm-4">
+                     <img class="img-responsive" src="img/locandina.jpg">
+                     <h4>Mad max: Fury road</h4>
+                     <h5>Action, adventure, Sci-Fi</h5>
+                 </div>
+                <div class="col-sm-4">
+                     <img class="img-responsive" src="img/locandina.jpg">
+                     <h4>Mad max: Fury road</h4>
+                     <h5>Action, adventure, Sci-Fi</h5>
+                 </div>
+                <div class="col-sm-4">
+                     <img class="img-responsive" src="img/locandina.jpg">
+                     <h4>Mad max: Fury road</h4>
+                     <h5>Action, adventure, Sci-Fi</h5>
+                 </div>
+             </div>
+        </div>
+    </main>
+    <div></div>
+    <footer>
+        <a><img src="#"></a>
+        <a><img src="#"></a>
+        <a><img src="#"></a>
+        <a><img src="#"></a>
+        <a><img src="#"></a>
+    </footer>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="node_modules/jquery/dist/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+
+  </body>
+</html>
