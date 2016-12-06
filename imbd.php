512ee90383c4e0ea2575c68f4d5980e0a4867d56
DIR\sebastiano.mura <murasebastiano87@gmail.com>
2016-12-06 17:50:51 +0100

    Quasi completata parte HTML e prima stilizzazione tramite Less

--- imbd.php
+++ imbd.php
@@ -13,6 +13,10 @@
 
         <!--additional fonts-->
         <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
+
+        <!--font awesome-->
+        <link rel="stylesheet" href="font-awesome-4.7.0/css/font-awesome.min.css">
 
            <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
         <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -22,88 +26,143 @@
         <![endif]-->
     </head>
   <body>
-    <header>          
-        <div class="container-fluid">
-            <nav class="navbar navbar-default">
+    <header>  
+        <nav class="navbar navbar-inverse navbar-static-top" role="navigation">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#"><img class="img-responsive" alt="Brand" src="img/imbd-logo.jpg"> </a>
                 <div class="navbar-header">
-                    <a class="navbar-brand" href="#">
-                        <img class="img-responsive" alt="Brand" src="img/imbd-logo.jpg"> 
-                    </a>
-                    <ul>
-                        <li><a>Movies</a></li>
-                        <li><a>Celebs & Photos</a></li>
-                        <li><a>Community</a></li>
-                        <li><a>News</a></li>
-                    </ul>
-                </div>       
-            </nav>
-        </div>
- 
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                </div>
 
-        <div class="title text-left">
-            <h3>The martian</h3>
-            <h4>Adventure Sci-fi Thriller</h4>
-            <!--bottone con modale-->
-            <button>trailer</button>
-            <p>In Theaters <br>
-            <span>15, Oct 2015 (USA)</span>
-            </p>
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav">
+                        <li><a href="index.php">Movies</a></li>
+                        <li><a href="about.php">Celebs & Photos</a></li>
+                        <li><a href="#portfolio">Community</a></li>
+                        <li><a href="#">News</a></li>
+                    </ul>
+                    <ul class="profile navbar-right">
+                        <li><a><i class="fa fa-cog" aria-hidden="true"></i></a></li>
+                        <li><img src="img/profile.png"></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <div class="title">
+        <h3>The martian</h3>
+        <a href="#">Adventure</a> <a href="#">Sci-fi</a>  <a href="#">Thriller</a> <br>
+        <!--bottone con modale-->
+        <button class="trailer-btn">Watch Trailer <i class="fa fa-play-circle-o" aria-hidden="true"></i></button>
+        <p>In Theaters <br>
+        <span>15, Oct 2015 (USA)</span>
+        </p>
         </div>
     </header>
 
     <main class="main-selection">
         <div class="container selection">
+            <nav class="navbar">
+                <ul class="nav navbar-nav">
+                    <li><a href="#">In Theaters</a></li>
+                    <li><a href="#">Coming Soon</a></li>
+                    <li><a href="#">Charts</a></li>
+                    <li><a href="#">Tv series</a></li>
+                    <li><a href="#">trailers</a></li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">more <span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                        <li><a href="#">Action</a></li>
+                        <li><a href="#">Another action</a></li>
+                        <li><a href="#">Something else here</a></li>
+                        <li><a href="#">Separated link</a></li>
+                        </ul>
+                    </li>
+                </ul>
+                <button class="trailer-btn">
+                <i class="fa fa-star" aria-hidden="true"><span>179</span></i>
+                </button>  
+            </nav>
             <div class="row">
-                <div class="navbar">
-                 <ul>
-                  <li>In theaters<li>
-                  <li>Coming soon<li>
-                  <li>Charts<li>
-                  <li>TV Series<li>
-                  <li>Trailers<li>
-                  <li>More</li>
+                <div class="col-sm-8 filters">
+                    <a><i class="fa fa-bars fa-2x" aria-hidden="true"></i></a>
+                    <a><i class="glyphicon glyphicon-th fa-2x" aria-hidden="true"></i></a>
                 </div>
-                <div class="white"></div>
-                <div class="col-sm-4">
-                     <img class="img-responsive" src="img/locandina.jpg">
-                     <h4>Mad max: Fury road</h4>
-                     <h5>Action, adventure, Sci-Fi</h5>
+                <div class="col-sm-4 white input-group">
+                    <input type="text" class="  search-query form-control" placeholder="Search" />
+                        <span class="input-group-btn">
+                            <button class="btn btn-danger" type="button">
+                                    <span class=" glyphicon glyphicon-search"></span>
+                            </button>
+                        </span> 
                 </div>
-                <div class="col-sm-4">
+                <div class="col-sm-4 box">
                      <img class="img-responsive" src="img/locandina.jpg">
                      <h4>Mad max: Fury road</h4>
-                     <h5>Action, adventure, Sci-Fi</h5>
-                 </div>
-                <div class="col-sm-4">
+                     <a href="#">Adventure</a> <a href="#">Sci-fi</a>  <a href="#">Action</a> <br>
+                     <i class="fa fa-heart fa-lg" aria-hidden="true"><span>9</span></i>
+                </div>
+                <div class="col-sm-4 box">
                      <img class="img-responsive" src="img/locandina.jpg">
-                     <h4>Mad max: Fury road</h4>
-                     <h5>Action, adventure, Sci-Fi</h5>
-                 </div>
-                <div class="col-sm-4">
+                     <h4>The Hunger Games</h4>
+                     <a href="#">Adventure</a> <a href="#">Sci-fi</a>  <a href="#">Action</a> <br>
+                     <i class="fa fa-heart fa-lg" aria-hidden="true"><span>8.7</span></i>
+                </div>
+                <div class="col-sm-4 box">
                      <img class="img-responsive" src="img/locandina.jpg">
-                     <h4>Mad max: Fury road</h4>
-                     <h5>Action, adventure, Sci-Fi</h5>
-                 </div>
-                <div class="col-sm-4">
+                     <h4>Jurassic World</h4>
+                     <a href="#">Adventure</a> <a href="#">Sci-fi</a>  <a href="#">Action</a> <br>
+                     <i class="fa fa-heart fa-lg" aria-hidden="true"><span>6.8</span></i>
+                </div>
+                <div class="col-sm-4 box">
                      <img class="img-responsive" src="img/locandina.jpg">
-                     <h4>Mad max: Fury road</h4>
-                     <h5>Action, adventure, Sci-Fi</h5>
-                 </div>
-                <div class="col-sm-4">
+                     <h4>Everest</h4>
+                     <a href="#">Adventure</a> <a href="#">Sci-fi</a>  <a href="#">Action</a> <br>
+                     <i class="fa fa-heart fa-lg" aria-hidden="true"><span>6.8</span></i>
+                </div>
+                <div class="col-sm-4 box">
                      <img class="img-responsive" src="img/locandina.jpg">
-                     <h4>Mad max: Fury road</h4>
-                     <h5>Action, adventure, Sci-Fi</h5>
+                     <h4>Insurgent</h4>
+                     <a href="#">Adventure</a> <a href="#">Sci-fi</a>  <a href="#">Action</a> <br>
+                     <i class="fa fa-heart fa-lg" aria-hidden="true"><span>7</span></i>
                  </div>
+                <div class="col-sm-4 box">
+                     <img class="img-responsive" src="img/locandina.jpg">
+                     <h4>Sicario</h4>
+                     <a href="#">Adventure</a> <a href="#">Sci-fi</a>  <a href="#">Action</a> <br>
+                     <i class="fa fa-heart fa-lg" aria-hidden="true"><span>9</span></i>
+                </div>
+                <div class="also">
+                    <i class="fa fa-spinner fa-4x" aria-hidden="true"></i>
+                </div>
              </div>
         </div>
     </main>
     <div></div>
     <footer>
-        <a><img src="#"></a>
-        <a><img src="#"></a>
-        <a><img src="#"></a>
-        <a><img src="#"></a>
-        <a><img src="#"></a>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-sm-4 text-left">  
+                    <a href="#"><img src="img/imbd.png"></a>
+                </div> 
+                <div class="col-sm-4 text-center mid"> 
+                    <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                    <a href="#"><i class="fa fa-facebook-square" aria-hidden="true"></i></a>
+                    <a href=""><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                </div>
+                <div class="col-sm-4 text-right inc"> 
+                    <span>1990-2015.imbd.com, inc.</span>
+                </div>
+                <div class="col-sm-12 also">
+                    <a href="#">show menu</a>
+                </div>
+            </div>
+        </div>
     </footer>
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="node_modules/jquery/dist/jquery.min.js"></script>
